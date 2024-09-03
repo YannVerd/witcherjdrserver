e6f236a286240964acdfa506f2cd45475c27aac8
YannV <yann.e.verdier@gmail.com>
2024-09-03 12:49:07 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+
+let __dirname = path.resolve();
+const app = express();
+
+
+
+app.use(cors());
+
+app.get('/grimoire', (req, res)=> {
+    res.header("Content-Type",'application/json');
+    res.sendFile(path.join(__dirname+"/datas/grimoire.json")); 
+})
+
+app.get("/character", (req, res)=>{
+
+})
+app.post("/character", (req, res)=>{
+
+})
+app.listen(3000, () => {  console.log("Serveur prêt")})
+
