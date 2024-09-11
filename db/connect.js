a3de7d40f1ab52ecee7b367a155bbfe42c6efbb5
YannV <yann.e.verdier@gmail.com>
2024-09-11 14:14:08 +0000

    save and replace character

---
+++ db/connect.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import dotenv from 'dotenv';
+import { MongoClient } from "mongodb";
+
+    export const connect = async () => {
+        const url = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+        const client = new MongoClient(url);
+
+        let conn;
+        let db;
+    try {
+        console.log("Connecting to the database...");
+        conn = await client.connect();
+        console.log("connection to the database established")
+        db = conn.db(process.env.DB_NAME);
+        
+        
+    } catch(e) {
+        console.error(e);
+    }
+
+    return db;
+}
--- index.js
+++ index.js
@@ -1,24 +1,64 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import dotenv from 'dotenv';
+import { connect } from './db/connect.js'
+import { ObjectId } from 'mongodb';
+
+
+dotenv.config();
+
+const db = await connect();
 
 let __dirname = path.resolve();
 const app = express();
 
-
-
 app.use(cors());
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 app.get('/grimoire', (req, res)=> {
     res.header("Content-Type",'application/json');
     res.sendFile(path.join(__dirname+"/datas/grimoire.json")); 
 })
 
-app.get("/character", (req, res)=>{
-
+app.get("/characters", async (req, res)=>{
+    try {
+        let datas = await db.collection("characters").find().toArray();
+        console.log(datas);
+        res.json(datas);
+        db.close();
+    }catch(e){
+        console.error('fail get characters', e)
+        res.send({message: "failed to get characters"})
+    }
 })
-app.post("/character", (req, res)=>{
-
+app.post("/character", async (req, res)=>{
+    console.log(req.body)
+    try {
+        if(!req.body._id){
+            let id ="";
+            await db.collection("characters").insertOne(req.body)
+                .then(result => {
+                        id = result.insertedId;      
+                });
+            res.status(200).send({message: "character saved", _id: id});
+        } else {
+            let dto = {}
+            for(let key in req.body){
+                if(key != "_id"){
+                    dto[key] = req.body[key]
+                }
+            }
+            await db.collection("characters").replaceOne({_id: ObjectId.createFromHexString(req.body._id)}, dto)
+            .then(res => console.log(res));
+            res.status(200).send({message: "character replace"});
+        } 
+    } catch (error) {
+        console.error("fail to insert/update character", error)
+        res.send({message: "error in the request"})
+    }
 })
 
 app.listen(3000, () => {  console.log("Serveur prêt")})
